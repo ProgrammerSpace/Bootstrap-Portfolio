61b95c8d610348bfafa13711323527c29a657405
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-07-13 22:06:03 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 $('.slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    cssEase: 'linear'
 });
